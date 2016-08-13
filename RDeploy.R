ecd248e7a576c2ec5672b039ff0136cde668d2bb
monchewharry <caodingxian@gmail.com>
2016-08-13 17:32:23 +0800

    confidential

--- RDeploy.R
+++ RDeploy.R
@@ -1,7 +1,7 @@
 library(rsconnect)
-setwd("/Users/CDX/R_workspace/Tradeinfo")
-rsconnect::setAccountInfo(name='ding', token='48CD5247CA4348EE301101517285A152'
-                          , secret='laBhGpWiW2RPKAiCGOLb3zMNfDY/K3LGMpbB267H')
+setwd("path to/Tradeinfo")
+rsconnect::setAccountInfo(name='xxx', token='xxxx'
+                          , secret='xxxxx')
 rsconnect::deployApp(account = 'ding',appName = 'Tradeinfo')
 
 library(shiny)
